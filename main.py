203714747ff7ac3f1a24a9dcf19d77be7a2b1ea1
hanieith <shkutkoff@gmail.com>
2022-07-03 21:32:01 +0300

    03.07

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
-@app.route("/index")
+# @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html", title='Index', menu=menu)
+    return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
@@ -16,14 +18,22 @@ def about():
     return render_template("about.html", title="About")
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form['username'])
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username="selfedu"))
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+# @app.route("/profile/<username>")
+# def profile(username):
+#   return f"Пользователь: {username}"
+
+# with app.test_request_context():
+#   print(url_for('index'))
+#  print(url_for('about'))
+# print(url_for('profile', username="selfedu"))
 
 
 if __name__ == "__main__":
