9ba5a7db05ab0c050eb0fe680b905fe878924edd
hanieith <shkutkoff@gmail.com>
2022-07-03 18:47:34 +0300

    03.07

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = ["Установка", "Первое приложение", "Обратная связь"]
+
 
 @app.route("/index")
 @app.route("/")
@@ -13,6 +14,16 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username="selfedu"))
 
 
 if __name__ == "__main__":
