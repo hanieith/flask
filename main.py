b9493a602d595de7fff84c8060cd2fd443f3c47f
hanieith <shkutkoff@gmail.com>
2022-07-03 21:54:02 +0300

    03.07

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hsdfhhasjfhj3hj2fhjksdnc_32'
+
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -21,8 +23,10 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form['username'])
-
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category="error")
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
 
