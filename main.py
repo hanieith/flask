928a127cc01cf8188787dafd8ea0c0d61f61e9a3
hanieith <shkutkoff@gmail.com>
2022-07-04 09:03:56 +0300

    04.07

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hsdfhhasjfhj3hj2fhjksdnc_32'
-
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -28,6 +27,30 @@ def contact():
         else:
             flash('Ошибка отправки', category="error")
     return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Пользователь: {username}"
 
 
 # @app.route("/profile/<username>")
